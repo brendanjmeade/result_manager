484aa6f07215b1f1f85fe9462ed3dedb34d2c2f3
Brendan Meade <brendanjmeade@users.noreply.github.com>
2024-09-03 21:31:02 -0400

    Adding a setup files for local install

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+version = "0.0.1"
+
+setup(
+    packages=["result_manager"],
+    install_requires=[],
+    zip_safe=False,
+    name="result_manager",
+    version=version,
+    description="Graphical output visualization for celeri",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/brendanjmeade/result_manager",
+    license="MIT",
+    platforms=["any"],
+    classifiers=[],
+)
